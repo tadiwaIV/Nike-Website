9e24c6075edbc82c72027f7e875a99ac6f2bbd6a
tadiwaIV <tadiwachivhere@gmail.com>
2021-07-06 14:29:46 +0200

    Initial commit

---
+++ sideNav.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function openOverlay() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeOverlay() {
+  document.getElementById("myNav").style.width = "0%";
+}
